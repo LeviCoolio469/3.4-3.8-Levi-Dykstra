dca67762e73fbd9e11528530e407c2abb794a849
LeviCoolio469 <levid.22060@obhs.school.nz>
2026-03-24 13:25:43 +1300

    first site setup doing

---
+++ responsive_nav.js
@@ -0,0 +1,12 @@
+// This is my responsive navigation changing into phone view
+
+alert("js is attached")
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
